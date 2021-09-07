82e9776a0078d7c0577fcfec81c947f24bfe3d97
Ulas Turkmen <ulas.tuerkmen@gmail.com>
2021-09-07 12:15:41 +0200

    Save feed xml, tests

--- sitegen/feeds.py
+++ sitegen/feeds.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 
 import rfeed
@@ -13,10 +14,14 @@ class FeedGenerator:
 
     def render(self, config, public_dir):
         feed_content = self.generate_feed(config)
+        os.makedirs(public_dir, exist_ok=True)
+        with open(os.path.join(public_dir, 'rss.xml'), 'w') as feed_file:
+            feed_file.write(self.generate_feed(config))
+
 
     def generate_feed(self, config):
         items = []
-        for cf in self.content_files:
+        for cf in sorted(self.content_files, key=lambda x: x.publish_date, reverse=True):
             url = furl(config['site']['url']) / cf.web_path
             item = rfeed.Item(title=cf.properties['title'],
                               link=url,
--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -1,5 +1,6 @@
 import unittest
 import tempfile
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import feedparser
@@ -25,4 +26,40 @@ class ContentContextTests(unittest.TestCase, CollectionTestBase):
         parsed = feedparser.parse(feed_xml)
         assert parsed.feed.title == "Test Site RSS Feed"
         assert len(parsed.entries) == 3
-        assert 'The Litany' in [x.title for x in parsed.entries]
+        titles = set(x.title for x in parsed.entries)
+        assert titles == set(['The Litany', 'The Tutorial', 'The Entry'])
+
+
+    def test_order_by_date(self):
+        fg = FeedGenerator()
+        now = datetime.now().replace(second=0, microsecond=0)
+        # Let's insert these out of order
+        fg.append_content_file(self.make_content_file('litany', 'the-litany', 'The Litany',
+                                                      date=now - timedelta(hours=1)))
+        fg.append_content_file(self.make_content_file('blog', 'the-entry', 'The Entry',
+                                                      date=now + timedelta(hours=1)))
+        fg.append_content_file(self.make_content_file('tutorial', 'the-tutorial', 'The Tutorial', date=now))
+        feed_xml = fg.generate_feed({'site': {'title': 'Test Site', 'url': 'https://bb.com', 'author': 'U T'}})
+        parsed = feedparser.parse(feed_xml)
+        assert parsed.entries[0].title == 'The Entry'
+        assert parsed.entries[1].title == 'The Tutorial'
+        assert parsed.entries[2].title == 'The Litany'
+
+
+    def test_render(self):
+        fg = FeedGenerator()
+        now = datetime.now().replace(second=0, microsecond=0)
+        # Let's insert these out of order
+        fg.append_content_file(self.make_content_file('litany', 'the-litany', 'The Litany',
+                                                      date=now - timedelta(hours=1)))
+        fg.append_content_file(self.make_content_file('blog', 'the-entry', 'The Entry',
+                                                      date=now + timedelta(hours=1)))
+        fg.append_content_file(self.make_content_file('tutorial', 'the-tutorial', 'The Tutorial', date=now))
+        public_dir = Path(self.workdir.name) / 'public'
+        fg.render({'site': {'title': 'Test Site', 'url': 'https://bb.com', 'author': 'U T'}},
+                  str(public_dir))
+        with open(public_dir / 'rss.xml', 'r') as feed_file:
+            feed_xml = feed_file.read()
+        parsed = feedparser.parse(feed_xml)
+        assert parsed.feed.title == "Test Site RSS Feed"
+        assert len(parsed.entries) == 3
